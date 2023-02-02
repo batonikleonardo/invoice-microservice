8621b3fcb50225afa748d9cdc124d3d265401c45
poweredbychocolate <dawid.brelak@gmail.com>
2023-02-02 21:51:01 +0100

    feat: app structure and config

---
+++ src/test/java/pl/batonikleonardo/invoicemicroservice/InvoiceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.batonikleonardo.invoicemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
